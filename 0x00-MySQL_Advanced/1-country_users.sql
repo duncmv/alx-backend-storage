a547645578168f7af6b8271d4bad1b2bb9c58233
duncmv <dtindiwensi@gmail.com>
2024-06-13 09:39:35 +0300

    task 1

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -3,5 +3,5 @@ CREATE TABLE IF NOT EXISTS users (
     id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email varchar(255) NOT NULL UNIQUE,
     name varchar(255),
-    country ENUM('US', 'CO', 'TN'),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
