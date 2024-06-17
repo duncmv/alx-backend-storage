d7bac2f0a495390dbab5e1228fe41193fa87fb36
duncmv <dtindiwensi@gmail.com>
2024-06-18 02:48:52 +0300

    basic pymongo

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -6,11 +6,11 @@ from pymongo import MongoClient
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
-    print("{} logs".format(nginx_collection.count_documents({})))
+    print("{} logs".format(nginx_collection.count({})))
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
         print("\tmethod {}: {}".format(
-            method, nginx_collection.count_documents({"method": method})))
+            method, nginx_collection.count({"method": method})))
     print("{} status check".format(
-        nginx_collection.count_documents({"method": "GET", "path": "/status"})))
+        nginx_collection.count({"method": "GET", "path": "/status"})))
