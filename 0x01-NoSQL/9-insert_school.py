ff4954e755a92b49dc301ed9db0fe1ae1f2a002e
duncmv <dtindiwensi@gmail.com>
2024-06-18 02:44:35 +0300

    basic pymongo

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -4,4 +4,4 @@
 
 def insert_school(mongo_collection, **kwargs):
     """inserts a new document in a collection based on kwargs"""
-    return mongo_collection.insert_one(kwargs)
+    return mongo_collection.insert(kwargs)
