d2d2029c0b93dcbce30a3ba0ceae03be20504bba
duncmv <dtindiwensi@gmail.com>
2024-06-19 17:32:02 +0300

    tasks 0-2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -10,7 +10,7 @@ def count_calls(method: Callable) -> Callable:
     """Decorator that counts the number of times a method is called"""
 
     @wraps(method)
-    def wrapper(self: 'Cache', *args, **kwargs):
+    def wrapper(self: object, *args, **kwargs):
         """Wrapper that increments the number of times a method is called"""
         self._redis.incr(method.__qualname__)
         return method(*args, **kwargs)
